063886f8b2760eecaa7c9672cac62e51f3fd306c
ariennss <arianna.franchetto@gmail.com>
2026-02-02 21:16:18 +0100

    added real API call since i have deployed it

--- JS/handleConversation.js
+++ JS/handleConversation.js
@@ -2,77 +2,71 @@
 // message objects include the message text and its origin (sender/bot)
 let messageQueue = [];
 
-
 // main function
 async function handleConversation(userInput) {
-    // let currentChat = chats.find(chat => chat.chatid == currentChatId);
-    // if (currentChat.chatContent.length == 0){
-        
-    // }
-
-    inputBar.value = "";
-    handleMessage(userInput, true);
-    let response = await getResponseFromHuggingFace(userInput);
-    handleMessage(response, false);
+  inputBar.value = "";
+  handleMessage(userInput, true);
+  let response = await getResponseFromHuggingFace(userInput);
+  handleMessage(response, false);
 }
 
-function handleMessage(messageFromUser, isFromUser){
-    let messageObject = buildMessageObject(messageFromUser, isFromUser);
-    addMessageToQueue(messageObject);
-    showMessages(messageObject);
+function handleMessage(messageFromUser, isFromUser) {
+  let messageObject = buildMessageObject(messageFromUser, isFromUser);
+  addMessageToQueue(messageObject);
+  showMessages(messageObject);
 }
 
 // call backend
-async function getResponseFromHuggingFace(userQuestion){
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+async function getResponseFromHuggingFace(userQuestion) {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-    const raw = JSON.stringify({
-        "message": userQuestion
-    });
+  const raw = JSON.stringify({
+    message: userQuestion,
+  });
 
-    const requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow"
-    };
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
 
-    console.log("POST body:" + JSON.stringify(requestOptions));
+  console.log("POST body:" + JSON.stringify(requestOptions));
 
-    const answer = await fetch("http://localhost:3000/sendMessageToHuggingFace", requestOptions);
-    const json = await answer.json();
-    
-        console.log(json.answer);
-    return json.answer;
+  const answer = await fetch(
+    "https://lying-chatbot-backend.onrender.com/sendMessageToHuggingFace",
+    requestOptions,
+  );
+  const json = await answer.json();
+
+  console.log(json.answer);
+  return json.answer;
 }
 
-function buildMessageObject(message, isFromUser){
-    return {
-        "message": message,
-        "isFromUser": isFromUser ? true : false
-    }
+function buildMessageObject(message, isFromUser) {
+  return {
+    message: message,
+    isFromUser: isFromUser ? true : false,
+  };
 }
 
 // push messages in the queue
-function addMessageToQueue(messageObject){
-    messageQueue.push(messageObject);
+function addMessageToQueue(messageObject) {
+  messageQueue.push(messageObject);
 }
 
 // show messages at frontend
-function showMessages(messageObject){
-   
-        const p = document.createElement("p");
-        addStyle(messageObject, p);
-        chatBody.appendChild(p);
-        p.innerText = messageObject.message;
-        chatBody.scrollTop = chatBody.scrollHeight;
-    ;
+function showMessages(messageObject) {
+  const p = document.createElement("p");
+  addStyle(messageObject, p);
+  chatBody.appendChild(p);
+  p.innerText = messageObject.message;
+  chatBody.scrollTop = chatBody.scrollHeight;
 }
 
-function addStyle(messageObject, messageParagraph){
-    let pClass = messageObject.isFromUser ? "user_messages" : "bot_messages";
-    messageParagraph.classList.add(pClass);
-    messageParagraph.classList.add("message");
+function addStyle(messageObject, messageParagraph) {
+  let pClass = messageObject.isFromUser ? "user_messages" : "bot_messages";
+  messageParagraph.classList.add(pClass);
+  messageParagraph.classList.add("message");
 }
-
--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-// API 
+// API
 const serverUrl = "http://localhost:3000";
 const endpoint = "sendMessageToHuggingFace";
 
@@ -15,10 +15,10 @@ const sidebar = document.getElementById("sidebar");
 
 // LOGIC
 const chats = [
-    {
-        chatid: crypto.randomUUID(),
-        chatContent: messageQueue
-    }
+  {
+    chatid: crypto.randomUUID(),
+    chatContent: messageQueue,
+  },
 ];
 console.log(chats);
 let currentChatId = chats[0].chatid;
@@ -29,81 +29,80 @@ headerOpenCloseButton.addEventListener("click", changeSidebarVisibility);
 newChatButton.addEventListener("click", newChat);
 hamburgerMenuButton.addEventListener("click", changeSidebarVisibility);
 
-async function onSubmitMessage(event){
-    event.preventDefault();
-    const submitTarget = event.target;
-    const formData = new FormData(submitTarget);
-    const questionFromUser = formData.get("userinput");
-    console.log("User input: " + questionFromUser);
-    chatPlaceholder.style.display = "none";
-    await handleConversation(questionFromUser);
+async function onSubmitMessage(event) {
+  event.preventDefault();
+  const submitTarget = event.target;
+  const formData = new FormData(submitTarget);
+  const questionFromUser = formData.get("userinput");
+  console.log("User input: " + questionFromUser);
+  chatPlaceholder.style.display = "none";
+  await handleConversation(questionFromUser);
 
-    if (chats.find(x => x.chatid === currentChatId).chatContent.length == 2){
-        chats.find(x => x.chatid === currentChatId).chatContent = [...messageQueue];
-        addNewChatToSidebar(currentChatId);
-    }
+  chats.find((x) => x.chatid === currentChatId).chatContent = [...messageQueue];
+  if (
+    chats.find((x) => x.chatid === currentChatId).chatContent.length == 2 &&
+    !document.getElementById(currentChatId)
+  ) {
+    addNewChatToSidebar(currentChatId);
+  }
 }
 
-function changeSidebarVisibility(){
-    if (sidebar.classList.contains("closed"))
-    {
-        sidebar.classList.remove("closed");
-        sidebar.classList.add("open");
-    }
-    else
-    {
-        sidebar.classList.remove("open");
-        sidebar.classList.add("closed");
-    }
+function changeSidebarVisibility() {
+  if (sidebar.classList.contains("closed")) {
+    sidebar.classList.remove("closed");
+    sidebar.classList.add("open");
+  } else {
+    sidebar.classList.remove("open");
+    sidebar.classList.add("closed");
+  }
 }
 
-function newChat(){
-    // old chat
-     chats.find(x => x.chatid === currentChatId).chatContent = [...messageQueue];
-     messageQueue = [];
-    // addNewChatToSidebar(currentChatId);
+function newChat() {
+  // old chat
+  chats.find((x) => x.chatid === currentChatId).chatContent = [...messageQueue];
+  messageQueue = [];
+  // addNewChatToSidebar(currentChatId);
 
-    // new chat
-    let guid = crypto.randomUUID();
-    currentChatId = guid;
-    chats.push({
-        chatid: guid,
-        chatContent: messageQueue
-    });
+  // new chat
+  let guid = crypto.randomUUID();
+  currentChatId = guid;
+  chats.push({
+    chatid: guid,
+    chatContent: messageQueue,
+  });
 
-    clearChatBody();
+  clearChatBody();
 }
 
-function addNewChatToSidebar(guid){
-    let liElement = document.createElement("li");
-    liElement.id = guid;
-    liElement.classList.add("chatListElement");
-    const firstMessage = chats.find(x => x.chatid == guid).chatContent[0].message;
-    liElement.innerText = firstMessage;
-    liElement.addEventListener("click", (e) => switchToChat(e) );
-    sidebarList.appendChild(liElement);
+function addNewChatToSidebar(guid) {
+  let liElement = document.createElement("li");
+  liElement.id = guid;
+  liElement.classList.add("chatListElement");
+  const firstMessage = chats.find((x) => x.chatid == guid).chatContent[0]
+    .message;
+  liElement.innerText = firstMessage;
+  liElement.addEventListener("click", (e) => switchToChat(e));
+  sidebarList.appendChild(liElement);
 }
 
-function clearChatBody(){
-    while (chatBody.firstChild){
-        chatBody.removeChild(chatBody.firstChild);
-    }
+function clearChatBody() {
+  while (chatBody.firstChild) {
+    chatBody.removeChild(chatBody.firstChild);
+  }
 }
 
-function switchToChat(event){
-    console.log(event);
-    const id = event.target.id;
-    currentChatId = id;
-    console.log(id);
+function switchToChat(event) {
+  console.log(event);
+  const id = event.target.id;
+  currentChatId = id;
+  console.log(id);
 
-    const chatToSwitchTo = chats.find(chat => chat.chatid == id);
-    console.log(chatToSwitchTo);
-    const chatToSwitchToMessages = chatToSwitchTo.chatContent;
-    console.log(chatToSwitchToMessages);
-    messageQueue = chatToSwitchToMessages;
-    console.log(messageQueue);
-    clearChatBody();
-    messageQueue.forEach(x => showMessages(x));
-
+  const chatToSwitchTo = chats.find((chat) => chat.chatid == id);
+  console.log(chatToSwitchTo);
+  const chatToSwitchToMessages = chatToSwitchTo.chatContent;
+  console.log(chatToSwitchToMessages);
+  messageQueue = chatToSwitchToMessages;
+  console.log(messageQueue);
+  clearChatBody();
+  messageQueue.forEach((x) => showMessages(x));
 }
-
